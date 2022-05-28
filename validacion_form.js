c92daec1123f5da7722336b885bc965d7f953dab
Valentina Zacaria <valentina.zacaria@gmail.com>
2022-05-27 22:37:54 -0300

    listopsubir

--- validacion_form.js
+++ validacion_form.js
@@ -4,30 +4,33 @@ const correo = document.getElementById("correo")
 const consulta = document.getElementById("consulta")
 const enviar = document.getElementById("enviar")
 
+//Función que valida que los campos de los inputs no estén vacios
 function camposNoVacios() {
-    if (nombre.value.lenght != 0 || 
-        correo.value.lenght != 0 ||
-        consulta.value.lenght != 0) {
+    if (nombre.value != "" ||
+        correo.value != "" ||
+        consulta.value != "") {
         return true
     } else {
         return false
     }
 }
 
-// function validarCorreo() {
-//     if (correo.value.includes("@") &&
-//         correo.value.includes(".com")) {
-//         return true
-//     } else {
-//         return false
-//     }
-// }
+//Función que valida el campo de correo electrónico
+function validarCorreo() {
+    if (correo.value.includes("@") &&
+        correo.value.includes(".com")) {
+        return true
+    } else {
+        return false
+    }
+}
 
 enviar.addEventListener("click", (e) => {
     e.preventDefault()
-    if (camposNoVacios() === true) {
+    if (validarCorreo() === true &&
+        camposNoVacios() === true) {
         alert("Gracias por contactarnos! ♥")
     } else {
-        alert("Complete los campos vacios")
+        alert("Correo electrónico con formato incorrecto o hay campos vacios")
     }
 })
